49355c5625f25cb6884deab1879918947dbd12eb
Harshal <103579668+Harshal-Khalkar@users.noreply.github.com>
2025-07-23 16:45:22 +0530

    Fix base path and homepage

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 export default defineConfig({
   plugins: [react()],
   base: '/my-portfolio/', 
-})
+});
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/my-portfolio/', 
+// })
 

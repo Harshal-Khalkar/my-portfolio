9d3b9908f52dc62d53fd6bd51787acdc334b4e21
Harshal Khalkar <harshalkhalkar0@gmail.com>
2026-07-30 12:21:38 +0530

    Update Vite base path for portfolio deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/', 
+  base: '/portfolio/',
 });
-
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // // https://vite.dev/config/
-// // export default defineConfig({
-// //   plugins: [react()],
-// // })
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/my-portfolio/', 
-// })
-
